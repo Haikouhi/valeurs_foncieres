a7100c31b932781a97e245048c98247b6f2d605d
RGuseynov <roustam@live.fr>
2019-05-22 14:56:22 +0200

    function de base

---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+def recuperation_ligne1(fichier_text):
+    with open(fichier_text, "r") as file:
+        ligne1 = file.readline().strip()
+    return ligne1
+
+
+def creation_dictionnaire(text_colonnes):
+    list_colonnes = text_colonnes.split("|")
+    dictionnaire = {}
+    for elem in list_colonnes:
+        dictionnaire[elem] = []
+    return dictionnaire
+
+
+def remplissage_dictionnaire(dictionnaire, fichier_text):
+    counter = 0
+    with open(fichier_text, "r") as file:
+        file.readline()
+        lines = file.readlines(1000)
+        for line in lines:
+            counter += 1
+            list_contenu_ligne = line.strip().split("|")
+            i = 0
+            for k, v in dictionnaire.items():
+                v.append(list_contenu_ligne[i])
+                i += 1
+    return counter
+
+
+dict = creation_dictionnaire(recuperation_ligne1("valeurs_foncieres/valeursfoncieres-2018.txt"))
+
+print(remplissage_dictionnaire(dict, "valeurs_foncieres/valeursfoncieres-2018.txt"))
+print(dict)
